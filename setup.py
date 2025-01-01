8e658f7d0c017c4a91162d0699b883eea75de8e9
Cloxl <Cloxl2017@outlook.at>
2025-01-02 01:04:44 +0800

    fix: 修复setup.py开源协议错误

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
 from setuptools import setup, find_packages
-
+import io
 setup(
     name="chatgpt-mirai-qq-bot-onebot-adapter",
     version="0.1.0",
@@ -10,12 +10,12 @@ setup(
     author="Cloxl",
     author_email="cloxl2017@outlook.at",
     description="OneBot adapter for lss233/chatgpt-mirai-qq-bot",
-    long_description=open("README.md").read(),
+    long_description=io.open("README.md", encoding='utf-8').read(),
     long_description_content_type="text/markdown",
     url="https://github.com/cloxl/chatgpt-mirai-qq-bot-onebot-adapter",
     classifiers=[
         "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: GNU Affero General Public License v3.0",
+        'License :: OSI Approved :: GNU Affero General Public License v3',
         "Operating System :: OS Independent",
     ],
 )
